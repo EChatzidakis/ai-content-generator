41aa0636a7ce91d54e299e35d108ca49f8661b87
EChatzidakis <stratos.chatzis@hotmail.com>
2025-03-12 15:11:32 +0100

    Refactor PageHeaderAvatar to conditionally render AvatarInitials for users without an image

--- src/components/PageHeader/PageHeaderAvatar.tsx
+++ src/components/PageHeader/PageHeaderAvatar.tsx
@@ -1,9 +1,8 @@
 'use client';
 import React from 'react';
-import Avatar from '../ui/Avatar';
-import Dropdown from '../ui/Dropdown';
 import { useAuthStore } from '../../store/auth/authStore';
 import { signOut } from 'next-auth/react';
+import { Avatar, AvatarInitials, Dropdown } from '../ui';
 
 const PageHeaderAvatar = () => {
   const user = useAuthStore((state) => state.user);
@@ -12,15 +11,22 @@ const PageHeaderAvatar = () => {
     return <React.Fragment />;
   }
 
+  const handleRenderAvatar = () => {
+    if (!user.image) {
+      return <AvatarInitials name={user.name} />;
+    }
+    const username = user.name;
+    const image = user.image || '/globe.svg';
+    return <Avatar name={username} src={image} />
+  };
+
   const handleSignOut = () => signOut();
   const signOutItem = <div onClick={handleSignOut}>Sign out</div>;
   const dropdownItems = [signOutItem];
 
-  const username = user.name;
-  const image = user.image || '/globe.svg';
   return (
     <Dropdown listItems={dropdownItems}>
-      <Avatar name={username} src={image} />
+      {handleRenderAvatar()}
     </Dropdown>
   );
 };
