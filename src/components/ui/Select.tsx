be0f06731ec3fa199542e8affba9a123f1dbe588
EChatzidakis <stratos.chatzis@hotmail.com>
2025-05-20 18:41:25 +0200

    Updates Select component to support option objects
    Changes the Select component to accept an array of option objects, where each object has a `label` and a `value` property, instead of a simple array of strings.
    This provides more flexibility and allows for displaying different labels than values.
    Also adds a `disabled` prop to allow disabling the select component.

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Select as MUISelect, MenuItem, FormControl, InputLabel } from '@mui/material';
 
+export interface SelectOption {
+  label: string;
+  value: string;
+}
+
 interface SelectProps {
-  options: string[];
+  options: SelectOption[];
   selected: string;
   onChange: (selected: string) => void;
   label?: string;
+  disabled?: boolean;
 }
 
-const SelectComponent: React.FC<SelectProps> = ({ options, selected, onChange, label }) => {
+const SelectComponent: React.FC<SelectProps> = ({ options, selected, onChange, label, disabled = false }) => {
   return (
     <FormControl fullWidth variant="outlined" size="small" sx={{ pb: 2 }}>
       {label && <InputLabel>{label}</InputLabel>}
@@ -16,10 +22,11 @@ const SelectComponent: React.FC<SelectProps> = ({ options, selected, onChange, l
         value={selected}
         label={label}
         onChange={(e) => onChange(e.target.value as string)}
+        disabled={disabled}
       >
         {options.map((option) => (
-          <MenuItem key={option} value={option}>
-            {option}
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
           </MenuItem>
         ))}
       </MUISelect>
