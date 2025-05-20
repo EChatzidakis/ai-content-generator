8932611e88afcbefab7212e218c463cf47adbe69
EChatzidakis <stratos.chatzis@hotmail.com>
2025-05-20 17:05:52 +0200

    Adds Zustand stores for content metadata
    Adds Zustand stores for managing content metadata, including audiences, categories, formats, tones and types.
    
    These stores provide a centralized way to fetch, store, and clear content metadata.
    This will be used to populate dropdowns and other UI elements related to content creation and management.

---
+++ src/store/contentTone/toneStore.ts
@@ -0,0 +1,33 @@
+import { create } from 'zustand';
+import { onGetAllTones } from '@/services/api/contentTone/contentToneApiCalls';
+import { ContentTone } from '@/types/content';
+
+type State = {
+  tones: ContentTone[];
+  loading: boolean;
+  error: boolean;
+  fetchTones: () => Promise<void>;
+  clearTones: () => void;
+};
+
+export const useAudienceStore = create<State>((set) => ({
+  tones: [],
+  loading: false,
+  error: false,
+
+  fetchTones: async () => {
+    set({ loading: true, error: false });
+    try {
+      const data = await onGetAllTones();
+      set({ tones: data, loading: false });
+    } catch (err: unknown) {
+      console.error('Error fetching tones:', err);
+      set({
+        error: true,
+        loading: false
+      });
+    }
+  },
+
+  clearTones: () => set({ tones: [] })
+}));
---
+++ src/store/contentType/typeStore.ts
@@ -0,0 +1,33 @@
+import { create } from 'zustand';
+import { onGetAllTypes } from '@/services/api/contentType/contentTypeApiCalls';
+import { ContentTone } from '@/types/content';
+
+type State = {
+  types: ContentTone[];
+  loading: boolean;
+  error: boolean;
+  fetchTypes: () => Promise<void>;
+  clearTypes: () => void;
+};
+
+export const useAudienceStore = create<State>((set) => ({
+  types: [],
+  loading: false,
+  error: false,
+
+  fetchTypes: async () => {
+    set({ loading: true, error: false });
+    try {
+      const data = await onGetAllTypes();
+      set({ types: data, loading: false });
+    } catch (err: unknown) {
+      console.error('Error fetching types:', err);
+      set({
+        error: true,
+        loading: false
+      });
+    }
+  },
+
+  clearTypes: () => set({ types: [] })
+}));
