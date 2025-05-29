a423a3b39143cefcd15eea5a6e29453490725db5
EChatzidakis <stratos.chatzis@hotmail.com>
2025-05-29 17:59:45 +0200

    Refactors content type interfaces
    Updates content type interfaces to better reflect data relationships.
    
    - Simplifies `ContentCategory` by nesting `ContentType` objects.
    - Introduces `ContentTypeNested` interface for nested content types.
    - Removes redundant `createdAt` and `updatedAt` properties from interfaces.
    - Modifies `ContentType` to include `categoryId` and allows empty arrays for `audiences`, `formats`, and `tones`.

--- src/types/content.ts
+++ src/types/content.ts
@@ -3,9 +3,17 @@ export interface ContentCategory {
     id: string;
     name: string;
     description: string;
-    types: ContentType[];
-    createdAt: string;
-    updatedAt: string;
+    types: ContentTypeNested[] | [];
+}
+
+export interface ContentTypeNested {
+  id: string;
+  name: string;
+  description: string;
+  categoryId: string;
+  defaultContentToneId: string;
+  defaultContentFormatId: string;
+  defaultAudienceId: string;
 }
 
 export interface ContentType {
@@ -17,38 +25,25 @@ export interface ContentType {
   defaultContentFormatId: string;
   defaultAudienceId: string;
 
-  createdAt: string;
-  updatedAt: string;
-
-  category: ContentCategory;
-
-  audiences: ContentAudience[];
-  formats: ContentFormat[];
-  tones: ContentTone[];
+  audiences: ContentAudience[] | [];
+  formats: ContentFormat[] | [];
+  tones: ContentTone[] | [];
 }
 
 export interface ContentAudience {
   id: string;
   name: string;
   description: string;
-  createdAt: string;
-  updatedAt: string;
 }
 
 export interface ContentFormat {
   id: string;
   name: string;
   description: string;
-  createdAt: string;
-  updatedAt: string;
-
-  types: object[];
 }
 
 export interface ContentTone {
   id: string;
   name: string;
   description: string;
-  createdAt: string;
-  updatedAt: string;
 }
